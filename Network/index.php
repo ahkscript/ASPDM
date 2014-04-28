9b8901c55edb225b82e8820ead737a73fedac65d
Joe DF <joedf@live.ca>
2014-04-28 02:32:59 -0400

    php cleanup

--- Network/index.php
+++ Network/index.php
@@ -229,33 +229,6 @@
 </html>
 
 ﻿<?php
-	function print_metadata($file,$content_item) {
-		if ($file == NULL) {
-			echo "ERROR: Invalid parameters";
-			return 0;
-		}
-		$file = "packs/" . str_replace("./","",$file);
-		if (!file_exists($file)) {
-			echo "ERROR: File does not exist.";
-			return 0;
-		}
-		$handle = fopen($file, "r");
-		fseek($handle,8);
-		$size = fread_UINT($handle);
-		if ($content_item==NULL){
-            $jsondata = fread($handle,$size);
-			echo $jsondata;
-        } else {
-			$obj = json_decode(fread($handle,$size));
-			$j_item = $obj->$content_item;
-			if (!strlen($j_item))
-				echo "ERROR: json item non-existent";
-			else
-				echo $j_item;
-		}
-		return $jsondata;
-	}
-	
 	function html_linefmt($str) {
 		$order   = array("\r\n", "\n", "\r");
 		$replace = '<br />';
@@ -286,33 +259,4 @@
 			return "<a href=\"http://creativecommons.org/licenses\">" . $str . "</a>";
 		return $str;
 	}
-	
-    function formatSizeUnits($bytes) {
-		if ($bytes >= 1073741824)
-		{
-			$bytes = number_format($bytes / 1073741824, 2) . ' GB';
-		}
-		elseif ($bytes >= 1048576)
-		{
-			$bytes = number_format($bytes / 1048576, 2) . ' MB';
-		}
-		elseif ($bytes >= 1024)
-		{
-			$bytes = number_format($bytes / 1024, 2) . ' KB';
-		}
-		elseif ($bytes > 1)
-		{
-			$bytes = $bytes . ' bytes';
-		}
-		elseif ($bytes == 1)
-		{
-			$bytes = $bytes . ' byte';
-		}
-		else
-		{
-			$bytes = '0 bytes';
-		}
-
-		return $bytes;
-    }
 ?>
