a063906cc9726a62edd10651b1312d708201a659
Joe DF <joedf@live.ca>
2014-06-15 00:12:36 -0400

    update preview download page

--- Network/client/index.php
+++ Network/client/index.php
@@ -1,8 +1,50 @@
 <?php
 	include '../lib/utils.php';
-	
-	echo "<h1>Not Ready Yet...</h1>";
-	echo '<a href="/">Click here for the homepage.</a>';
-	$version = iniget("update.ini","version");
-	echo '<hr><a href="ASPDM_Install-v'.$version.'.exe">Click here to download the preview release. [Version: '.$version.']</a>';
 ?>
+<html>
+<head>
+<title>Client | ASPDM</title>
+<style>
+	body {
+		font-family:Arial,sans-serif;
+		text-align:center;
+		margin:auto;
+	}
+	table { text-align:center;vertical-align:middle; }
+	#dnl{
+		border: solid 1px;
+		border-radius: 4px;
+		font-size:12px;
+		background-color:#E5E5E5;
+	}
+	#dnl td { vertical-align:middle;text-align:center; }
+	#content { margin-top:200px; }
+	#sp { margin:auto;text-align:center;width:300px; }
+	a:active { position:relative;top:1px; }
+</style>
+</head>
+<body>
+<div id="content">
+<?php
+	echo '<h1>ASPDM : Package Manager - Preview</h1><hr><br><br>';
+	
+	$version = iniget("update.ini","version");
+	echo '<div id="sp">';
+	echo '<a href="ASPDM_Install-v'.$version.'.exe" id="link">';
+?>
+		<table id="dnl">
+			<tr>
+				<td><img src="/src/install48.png" alt="Logo"></td>
+<?php
+				echo '<td>Click here to download the preview release.<br>[Version: '.$version.']</td>';
+?>
+			</tr>
+		</table>
+	</a>
+	</div>
+	
+	<br><br>
+	<hr><a href="/">Click here for the homepage.</a>
+<div>
+</body>
+</html>
