feea645b297f40183364af409de43cfa0995fcb8
Joe DF <joedf@live.ca>
2015-01-13 14:09:07 -0500

    fix invalid json in api/status.php

--- Network/api/status.php
+++ Network/api/status.php
@@ -2,5 +2,5 @@
 	$var = (isset($_REQUEST['_c']))?$_REQUEST['_c']:"NULL";
 	$var = str_replace(array('"',"'",'\\'),"",$var);
 	
-	print('{api:{name:"aspdm",check:"'.$var.'"}}');
+	print('{api:{"name":"aspdm","check":"'.$var.'"}}');
 ?>
