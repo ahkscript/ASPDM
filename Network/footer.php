5a6a1694a6f7b0ff3986dadaff5d32c29b0d47b6
Joe DF <joedf@live.ca>
2015-06-03 01:30:17 -0400

    remove 2fh icon : now hosted on ahkscript.org

--- Network/footer.php
+++ Network/footer.php
@@ -1,9 +1,5 @@
 <div id="footer">
-	<p>Hosted by <a href="http://www.2freehosting.com/"><img src="src/2fh_icon.png" alt="2FH"></a>
-	-
-	<a href="http://joedf.users.sourceforge.net/"> Joe DF</a>
-	-
-	Original Web Design from here: <a href="http://win32.libav.org/win64/">http://win32.libav.org/win64/</a>
+	<p>Original Web Design from here: <a href="http://win32.libav.org/win64/">http://win32.libav.org/win64/</a>
 	<span style="float:right;">AHKScript.org - <a href="http://ahkscript.org">Official Website</a></span>
 	</p>
 </div>
