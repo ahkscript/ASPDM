32177effcbe5dc48d720710d14dc9b2d115be1a9
Joe DF <joedf@live.ca>
2015-06-18 16:39:57 -0400

    Fix logout.php
    "headers already sent"

--- Network/logout.php
+++ Network/logout.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+include 'lib/server.php';
+sessionInvalid();
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -6,14 +11,6 @@
 		<title>Logout</title>
 	</head>
 	<body>
-		<?php
-		session_start();
-		
-		include 'lib/server.php';
-		sessionInvalid();
-		
-		?>
-
 		<h1>logged out.</h1>
 		<p>You will be redirected in <span id="counter">3</span> second(s).
 		<br><a href="/index.php">Click here to be redirected immediately.</a></p>
